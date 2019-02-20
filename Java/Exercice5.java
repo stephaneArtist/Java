eb20652b15efd871988a4dd7e6aa5cb9b9d63b56
Stephane <hollanderstephane5@gmail.com>
2019-02-20 10:38:14 +0100

    Modification de l'architecture des fichiers

---
+++ Java/Exercice5.java
@@ -0,0 +1,9 @@
+package exercice;
+
+public class Exercice5 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ MainDice.java
@@ -0,0 +1,48 @@
+package exercice;
+
+import java.util.Scanner;
+
+public class MainDice {
+
+    public static void main(String[] args) {
+
+        System.out.println("Parti B");
+        exercice.Dice dice = new exercice.Dice();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the number of throws :");
+        int iteration = Integer.parseInt(sc.next());
+
+        for(int i = 1 ; i < iteration+1 ; i++){
+
+            dice.launch();
+            System.out.println(" Game" +i+ " ");
+            System.out.println("Result = "+dice.getValue());
+        }
+
+        System.out.println();
+
+        System.out.println("Parti C");
+
+        exercice.Dice diceO = new exercice.Dice();
+        exercice.Dice diceT = new exercice.Dice();
+
+        sc = new Scanner(System.in);
+        System.out.println("Please enter the number of throws :");
+        iteration = Integer.parseInt(sc.next());
+
+        for (int i = 1 ; i < iteration + 1 ; i++){
+
+            diceO.launch();
+            diceT.launch();
+            System.out.println(" Game " +i+ " ");
+            System.out.println("Result Dic 1 = "+diceO.getValue()+"| Result Dic 2 = "+diceT.getValue());
+
+            if(diceO.getValue() == diceT.getValue()){
+
+                System.out.println("You have won");
+            }
+        }
+
+    }
+
+}
